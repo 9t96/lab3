dbe44d480256c3184bf35ee70ce436661cef4ef0
laloo96 <ohmycaio@gmail.com>
2017-06-29 22:41:30 -0300

    Filtrado por color practica parcial.

--- LABIII/PSP_LAB/Controladora.js
+++ LABIII/PSP_LAB/Controladora.js
@@ -9,19 +9,26 @@ var Entidades;
             var precio;
             var color;
             var miHidden = document.getElementById('hdnIdAuto').value;
-            if (miHidden !== "") {
-                marca = document.getElementById('txtMarca').value;
-                precio = Number(document.getElementById('txtPrecio').value);
-                color = document.getElementById('txtColor').value;
-                Controladora.EliminarAuto(Number(document.getElementById('miHidden').value));
-            }
+            marca = document.getElementById('txtMarca').value;
+            precio = Number(document.getElementById('txtPrecio').value);
+            color = document.getElementById('txtColor').value;
+            console.log(color);
             var auto1 = new Entidades.Auto(marca, color, precio);
+            //Traigo los autos para no pisar la info.
             var autosLS = JSON.parse(localStorage.getItem("autos"));
+            //Si no hay autos deja el array vacio.
             var autosJson = autosLS !== null ? autosLS : [];
+            //Transformo en json.
             autosJson.push(JSON.parse(auto1.ToJson()));
+            //Los guardo en localstroge.
             localStorage.setItem("autos", JSON.stringify(autosJson));
+            if (miHidden !== "") {
+                this.EliminarAuto(parseInt(miHidden));
+                this.MostrarAuto();
+            }
         };
         Controladora.MostrarAuto = function () {
+            var colorEnLetras;
             var autos = JSON.parse(localStorage.getItem("autos"));
             if (autos !== null) {
                 if (localStorage.length > 0) {
@@ -29,14 +36,27 @@ var Entidades;
                     tabla += "<table class='table table-default'>" +
                         "<tr><th>Marca</th><th>Color</th><th>Precio</th><th>Acciones</th></tr>";
                     for (var index = 0; index < autos.length; index++) {
-                        tabla += "<tr><td>" + autos[index].marca + "</td><td>" + autos[index].color + "</td><td>" + autos[index].precio + "</td><td><a class='btn btn-danger' onclick='Entidades.Controladora.ModificarAuto(" + index + ")'>Eliminar</a><a class='btn btn-warning' onclick='Entidades.Controladora.ModificarAuto(" + index + ")'>Modificar</a></tr>";
+                        switch (parseInt(autos[index].color)) {
+                            case 1:
+                                colorEnLetras = "Rojo";
+                                break;
+                            case 2:
+                                colorEnLetras = "Negro";
+                                break;
+                            case 3:
+                                colorEnLetras = "Blanco";
+                                break;
+                        }
+                        tabla += "<tr>" +
+                            "<td>" + autos[index].marca + "</td>" +
+                            "<td>" + colorEnLetras + "</td>" +
+                            "<td>" + autos[index].precio + "</td>" +
+                            "<td>" +
+                            "<a class='btn btn-danger' onclick='Entidades.Controladora.EliminarAuto(" + index + ")'>Eliminar</a>" +
+                            "<a class='btn btn-warning' onclick='Entidades.Controladora.ModificarAuto(" + index + ")'>Modificar</a>" +
+                            "</td>" +
+                            "</tr>";
                     }
-                    /*
-                    autos.forEach(element => {
-                        
-                        tabla+= "<tr><td>"+element.marca+"</td><td>"+element.color+"</td><td>"+element.precio+"</td><td><a class='btn btn-danger' onclick='Entidades.Controladora.EliminarAuto()'>Eliminar</a><a class='btn btn-warning'>Modificar</a></tr>";
-                        //console.log(element.marca+"-"+element.color+"-"+element.precio);
-                    });*/
                     tabla += "</table>";
                     document.getElementById('divTabla').innerHTML = tabla;
                 }
@@ -50,6 +70,10 @@ var Entidades;
             localStorage.setItem("autos", JSON.stringify(autosJson));
             Controladora.MostrarAuto();
         };
+        /**
+         * Llena los input con los datos de auto a modificar.
+         * @param i Index del auto a modificar.
+         */
         Controladora.ModificarAuto = function (i) {
             var autosLS = JSON.parse(localStorage.getItem("autos"));
             document.getElementById('txtMarca').value = autosLS[i].marca;
@@ -58,6 +82,41 @@ var Entidades;
             document.getElementById('hdnIdAuto').value = i.toString();
         };
         Controladora.FiltrarPorColor = function () {
+            var colorEnLetras;
+            var color = document.getElementById('txtColor').value;
+            var autos = JSON.parse(localStorage.getItem("autos"));
+            var autosXcolor = autos.filter(function (autos) { return autos.color == color; });
+            if (autosXcolor !== null) {
+                if (localStorage.length > 0) {
+                    var tabla = void 0;
+                    tabla += "<table class='table table-default'>" +
+                        "<tr><th>Marca</th><th>Color</th><th>Precio</th><th>Acciones</th></tr>";
+                    for (var index = 0; index < autosXcolor.length; index++) {
+                        switch (parseInt(autosXcolor[index].color)) {
+                            case 1:
+                                colorEnLetras = "Rojo";
+                                break;
+                            case 2:
+                                colorEnLetras = "Negro";
+                                break;
+                            case 3:
+                                colorEnLetras = "Blanco";
+                                break;
+                        }
+                        tabla += "<tr>" +
+                            "<td>" + autosXcolor[index].marca + "</td>" +
+                            "<td>" + colorEnLetras + "</td>" +
+                            "<td>" + autosXcolor[index].precio + "</td>" +
+                            "<td>" +
+                            "<a class='btn btn-danger' onclick='Entidades.Controladora.EliminarAuto(" + index + ")'>Eliminar</a>" +
+                            "<a class='btn btn-warning' onclick='Entidades.Controladora.ModificarAuto(" + index + ")'>Modificar</a>" +
+                            "</td>" +
+                            "</tr>";
+                    }
+                    tabla += "</table>";
+                    document.getElementById('divTabla').innerHTML = tabla;
+                }
+            }
         };
         return Controladora;
     }());
--- LABIII/PSP_LAB/Controladora.ts
+++ LABIII/PSP_LAB/Controladora.ts
@@ -10,33 +10,37 @@ namespace Entidades
             let color:string;
 
            let miHidden:string = (<HTMLInputElement>document.getElementById('hdnIdAuto')).value;
-          
-           if(miHidden !== "")       
-           {
-                marca   = (<HTMLInputElement>document.getElementById('txtMarca')).value;
-                precio  = Number((<HTMLInputElement>document.getElementById('txtPrecio')).value);
-                color   = (<HTMLInputElement> document.getElementById('txtColor')).value;
 
-                Controladora.EliminarAuto(Number((<HTMLInputElement>document.getElementById('miHidden')).value));              
-           }
+            marca   = (<HTMLInputElement>document.getElementById('txtMarca')).value;
+            precio  = Number((<HTMLInputElement>document.getElementById('txtPrecio')).value);
+            color   = (<HTMLInputElement> document.getElementById('txtColor')).value;
 
+            console.log(color);
 
-            
             let auto1:Auto = new Auto(marca,color,precio);
 
+            //Traigo los autos para no pisar la info.
             let autosLS = JSON.parse(localStorage.getItem("autos"));
-
+            //Si no hay autos deja el array vacio.
             let autosJson:Array<JSON> = autosLS !== null ? autosLS :[];
-
+            //Transformo en json.
             autosJson.push(JSON.parse(auto1.ToJson()));
-
+            //Los guardo en localstroge.
             localStorage.setItem("autos",JSON.stringify(autosJson));
+
+            if (miHidden !== "") {
+
+                this.EliminarAuto(parseInt(miHidden));    
+                this.MostrarAuto();      
+            }
                                    
         }
 
         public static MostrarAuto()
-        {
+        {   
+            let colorEnLetras:string;
             let autos = JSON.parse(localStorage.getItem("autos"));
+            
             if(autos !== null)
             {   
                if(localStorage.length > 0)
@@ -47,18 +51,31 @@ namespace Entidades
                         "<tr><th>Marca</th><th>Color</th><th>Precio</th><th>Acciones</th></tr>";
 
                     for (let index = 0; index < autos.length; index++) {
-                        
-                        tabla+= "<tr><td>"+autos[index].marca+"</td><td>"+autos[index].color+"</td><td>"+autos[index].precio+"</td><td><a class='btn btn-danger' onclick='Entidades.Controladora.ModificarAuto("+index+")'>Eliminar</a><a class='btn btn-warning' onclick='Entidades.Controladora.ModificarAuto("+index+")'>Modificar</a></tr>";
 
                         
+                        switch (parseInt(autos[index].color)) {
+                            case 1:
+                                colorEnLetras = "Rojo";
+                                break;
+                            case 2:
+                                colorEnLetras = "Negro";
+                                break;                      
+                            case 3:
+                                colorEnLetras = "Blanco";
+                                break;
+                        }
+                        
+                        tabla+= "<tr>"+
+                                "<td>"+autos[index].marca+"</td>"+
+                                "<td>"+colorEnLetras+"</td>"+
+                                "<td>"+autos[index].precio+"</td>"+
+                                "<td>"+
+                                "<a class='btn btn-danger' onclick='Entidades.Controladora.EliminarAuto("+index+")'>Eliminar</a>"+
+                                "<a class='btn btn-warning' onclick='Entidades.Controladora.ModificarAuto("+index+")'>Modificar</a>"+
+                                "</td>"+
+                                "</tr>";              
                     }
                     
-                    /*
-                    autos.forEach(element => {
-                        
-                        tabla+= "<tr><td>"+element.marca+"</td><td>"+element.color+"</td><td>"+element.precio+"</td><td><a class='btn btn-danger' onclick='Entidades.Controladora.EliminarAuto()'>Eliminar</a><a class='btn btn-warning'>Modificar</a></tr>";
-                        //console.log(element.marca+"-"+element.color+"-"+element.precio);
-                    });*/
 
                     tabla+="</table>";
 
@@ -84,6 +101,10 @@ namespace Entidades
 
         }
 
+        /**
+         * Llena los input con los datos de auto a modificar.
+         * @param i Index del auto a modificar.
+         */
         public static ModificarAuto(i:number):void
         {
             let autosLS = JSON.parse(localStorage.getItem("autos"));
@@ -96,7 +117,52 @@ namespace Entidades
         }
 
         public static FiltrarPorColor()
-        {
+        {   
+            let colorEnLetras:string;
+            let color = (<HTMLInputElement> document.getElementById('txtColor')).value;
+            let autos = JSON.parse(localStorage.getItem("autos"));
+
+            let autosXcolor = autos.filter(autos => autos.color == color);
+          
+            if(autosXcolor !== null)
+            {   
+               if(localStorage.length > 0)
+               {
+                    let tabla:string;
+
+                    tabla+="<table class='table table-default'>"+
+                        "<tr><th>Marca</th><th>Color</th><th>Precio</th><th>Acciones</th></tr>";
+
+                    for (let index = 0; index < autosXcolor.length; index++) {
+
+                        
+                        switch (parseInt(autosXcolor[index].color)) {
+                            case 1:
+                                colorEnLetras = "Rojo";
+                                break;
+                            case 2:
+                                colorEnLetras = "Negro";
+                                break;                      
+                            case 3:
+                                colorEnLetras = "Blanco";
+                                break;
+                        }
+                        
+                        tabla+= "<tr>"+
+                                "<td>"+autosXcolor[index].marca+"</td>"+
+                                "<td>"+colorEnLetras+"</td>"+
+                                "<td>"+autosXcolor[index].precio+"</td>"+
+                                "<td>"+
+                                "<a class='btn btn-danger' onclick='Entidades.Controladora.EliminarAuto("+index+")'>Eliminar</a>"+
+                                "<a class='btn btn-warning' onclick='Entidades.Controladora.ModificarAuto("+index+")'>Modificar</a>"+
+                                "</td>"+
+                                "</tr>";              
+                    }
+
+                    tabla+="</table>";
+
+                    (<HTMLInputElement> document.getElementById('divTabla')).innerHTML = tabla;
+               }
 
         }
 
